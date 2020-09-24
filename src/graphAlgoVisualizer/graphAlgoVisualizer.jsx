e72b493a2a2b095c0de29e328c8177e2732e19ec
kushgupta-official <kushgupta.potus@gmail.com>
2020-09-25 04:12:07 +0530

    Start and end nodes

--- src/graphAlgoVisualizer/graphAlgoVisualizer.jsx
+++ src/graphAlgoVisualizer/graphAlgoVisualizer.jsx
@@ -2,8 +2,12 @@ import React, { Component } from "react";
 import Node from "./node/node.jsx";
 import "./graphAlgoVisualizer.css";
 
-const total_rows = 2;
-const total_columns = 5;
+const total_rows = 20;
+const total_columns = 50;
+const startNode_Row = 10;
+const startNode_Col = 15;
+const endNode_Row = 10;
+const endNode_Col = 35;
 
 class GraphAlgoVisualizer extends Component {
   state = {
@@ -11,10 +15,16 @@ class GraphAlgoVisualizer extends Component {
   };
   componentDidMount() {
     const grid = [];
-    for (let i = 0; i < total_rows; i++) {
+    for (let row = 0; row < total_rows; row++) {
       const currentRow = [];
-      for (let j = 0; j < total_columns; j++) {
-        currentRow.push([]);
+      for (let column = 0; column < total_columns; column++) {
+        const currentNode = {
+          row,
+          column,
+          isStart: row === startNode_Row && column === startNode_Col,
+          isEnd: row === endNode_Row && column === endNode_Col,
+        };
+        currentRow.push(currentNode);
       }
       grid.push(currentRow);
     }
@@ -28,10 +38,19 @@ class GraphAlgoVisualizer extends Component {
         <div className="grid">
           {grid.map((row, rowIdx) => {
             return (
-              <div>
-                {row.map((node, nodeIdx) => (
-                  <Node />
-                ))}
+              <div key={rowIdx}>
+                {row.map((node, nodeIdx) => {
+                  const { row, column, isStart, isEnd } = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      row={row}
+                      column={column}
+                      isStart={isStart}
+                      isEnd={isEnd}
+                    ></Node>
+                  );
+                })}
               </div>
             );
           })}
--- src/graphAlgoVisualizer/node/node.jsx
+++ src/graphAlgoVisualizer/node/node.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import "./node.css";
-var t = 1;
+//var t = 1;
 
 class Node extends Component {
   state = {};
+
+  getClassName = () => {
+    let str = "node";
+    if (this.props.isStart === true) {
+      str = str + " startNode";
+      console.log("start");
+    } else if (this.props.isEnd === true) {
+      str = str + " endNode";
+      console.log("end");
+    }
+    return str;
+  };
+
   render() {
-    // console.log((t = t + 1));
-    return <div className="node"></div>;
+    return <div className={this.getClassName()}></div>;
   }
 }
 
